bf4830a5c9e640d7dcf089ccbaa237e755106dc9
protaknium <49377950+protaknium@users.noreply.github.com>
2019-05-31 00:53:36 +0300

    Create fi.lua

---
+++ locales/fi.lua
@@ -0,0 +1,7 @@
+Locales['fi'] = {
+  ['male'] = 'mies',
+  ['female'] = 'nainen',
+  ['carjack'] = '~o~Autoryöstö käynnissä~s~: a ~r~%s~s~ Nähtiin varastamassa : ~o~%s~s~  ~y~%s~s~',
+  ['combat'] = '~o~Tappelu käynnissä~s~: a ~r~%s~s~ Nähtiin tappelemassa : ~y~%s~s~',
+  ['gunshot'] = '~o~Ammuskelu käynnissä~s~: a ~r~%s~s~ Nähtiin ampumassa : ~y~%s~s~',
+}
